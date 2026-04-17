f591c08c8ac62f7de30b7919934458b97808ff4c
bushra-marwa <bushra613m@gmail.com>
2026-04-18 02:13:50 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -53,3 +53,9 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`)
 })
+
+const cors = require("cors");
+
+app.use(cors({
+  origin: "*"
+}));
